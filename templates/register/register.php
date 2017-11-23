90413f345332fe8ab970adf4c0745f0bfc95d480
David Reis <david.alex.reis.97@gmail.com>
2017-11-23 10:11:45 +0000

    Added register page

---
+++ templates/register/register.php
@@ -0,0 +1,13 @@
+<section id="registerForm">
+	<form action="action_register.php" method="post">
+		<label for="name">Name</label>
+		<input type="text" placeholder="name" name="name" id="name">
+		<label for="username">Username</label>			
+		<input type="text" placeholder="username" name="username" id="username">
+		<label for="password">Password</label>						
+		<input type="password" placeholder="password" name="password" id="password">
+		<label for="confirmPassword">Confirm Password</label>									
+		<input type="password" placeholder="confirm password" name="confirmPassword" id="confirmPassword">			
+	    <input type="submit" value="Register">
+	</form>
+</section>
