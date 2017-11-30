ff9b3882a41e48b3af1b7e0c12173c9785625d5c
ajcarv <up201506688@fe.up.pt>
2017-11-30 10:40:48 +0000

    add error message for login fail

--- action_login.php
+++ action_login.php
@@ -1,9 +1,12 @@
 <?php
 include_once('includes/init.php');
 $username = trim(strip_tags($_POST['username']));
-$password = $_POST['password'];  
+$password = $_POST['password'];
 if (isLoginCorrect($_POST['username'], $_POST['password'])) {
   setCurrentUser($_POST['username']);
   }
-header('Location: ' . $_SERVER['HTTP_REFERER']); 
+else{
+    $_SESSION['loginError'] = true;
+}
+header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
