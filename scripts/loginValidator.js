f4b5cfec27186174bf36b46fa1e39c6b8edf70e9
fbahamonde <felipe.bahamonde.m@ug.uchile.cl>
2017-12-13 17:36:31 +0000

    Login validator

---
+++ scripts/loginValidator.js
@@ -0,0 +1,38 @@
+let username = document.querySelector('#loginform input[name=username]');
+let password = document.querySelector('#loginform input[name=password]');
+let register = document.querySelector('#loginform input[type=submit]');
+password.addEventListener('keyup', validatePassword, false);
+username.addEventListener('keyup', validateUsername, false);
+register.addEventListener('click', validateRegister, false);
+
+let invalidPassword = true;
+let invalidUsername = true;
+
+function validateUsername(other){
+    if (!/^.*.{0,}$/.test(this.value))
+      invalidUsername = true;
+    else
+      invalidUsername = false;
+  }
+
+function validatePassword(other){
+  if (!/^.*(?=.*[A-Z])(?=.*[0-9]).{7,}$/.test(this.value))
+    invalidPassword = true;
+  else
+    invalidPassword = false;
+}
+
+
+function validateRegister(event){
+  if  (invalidPassword && invalidUsername){
+    event.preventDefault(); 
+    alert('Invalid or Missing Username and Password')
+  }
+  else if (invalidPassword || invalidUsername){
+    event.preventDefault();
+  if (invalidPassword)
+    alert('No valid Password');
+  if (invalidUsername)
+    alert('No valid Username');
+  }
+}
--- templates/common/login_form.php
+++ templates/common/login_form.php
@@ -1,14 +1,17 @@
-<form action="action_login.php" method="post">
+<script src="scripts/loginValidator.js" defer></script>
+<form action="action_login.php" method="post" id="loginform">
   <input type="text" placeholder="username" name="username">
   <input type="password" placeholder="password" name="password">
   <input type="submit" value="&gt;&gt;">
   <div>
     <a href="register.php">Register</a>
   </div>
+  
   <?php if (isset($_SESSION['loginError'])){
 			?><div id="error_login">
 				<p>Error username or password!</p>
 			</div><?php
 			unset($_SESSION['loginError']);
-		} ?>
+    } 
+    ?>
 </form>
